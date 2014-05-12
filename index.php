6236ba0ecc4428f1980df7e2c0a0c583f6d9390e
Jenn Schiffer <jenn@pancaketheorem.com>
2014-05-11 20:38:25 -0400

    initial homepage and page style

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php get_header(); ?>
 
-  <h1>my slides</h1>
+  <div class="page-container">
+    <header>
+      <h1><?php bloginfo('name'); ?></h1>
+      <h4><?php bloginfo('description'); ?></h4>
+    </header>
 
-  <?php if ( have_posts() ): ?>
-    <h2>Latest Slides</h2>
-      <ol>
-        <?php while ( have_posts() ) : the_post(); ?>
-        <li>
-          <article>
-            <h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-            <?php the_excerpt(); ?>
-          </article>
-        </li>
-        <?php endwhile; ?>
-      </ol>
-    <?php else: ?>
-    <h2>No slides to display</h2>
-  <?php endif; ?>
+      <?php if ( have_posts() ): ?>
+          <ol>
+            <?php while ( have_posts() ) : the_post(); ?>
+            <li>
+              <article>
+                <h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
+              </article>
+            </li>
+            <?php endwhile; ?>
+          </ol>
+        <?php else: ?>
+        <h2>No slides to display</h2>
+      <?php endif; ?>
 
+    <footer>
+      <p>&copy; <?php echo date('Y') . ' ' . bloginfo('name') . ' &bull; <a href="#">SimpleSlides</a> theme by <a href="http://jennmoney.biz">j$</a>'; ?></p>
+    </footer>
+  </div>
 <?php get_footer(); ?>
